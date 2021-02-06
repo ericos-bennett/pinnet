89a84f1966d54e1a1a866777018d059aab3cf131
QuintenAiton <quinaiton@hotmail.com>
2021-02-06 22:46:49 +0000

    Seeds User DB with Users

---
+++ db/seeds/userSeeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (email, password, profile_picture)VALUES('Wilma_Hilpert47@gmail.com', 'password', 'https://s3.amazonaws.com/uifaces/faces/twitter/larrygerard/128.jpg');
+INSERT INTO users (email, password, profile_picture)VALUES('Wilma_Hilpert47@gmail.com', 'password', 'https://s3.amazonaws.com/uifaces/faces/twitter/larrygerard/128.jpg');
+INSERT INTO users (email, password, profile_picture)VALUES('Wilma_Hilpert47@gmail.com', 'password', 'https://s3.amazonaws.com/uifaces/faces/twitter/larrygerard/128.jpg');
+INSERT INTO users (email, password, profile_picture)VALUES('Wilma_Hilpert47@gmail.com', 'password', 'https://s3.amazonaws.com/uifaces/faces/twitter/larrygerard/128.jpg');
+INSERT INTO users (email, password, profile_picture)VALUES('Wilma_Hilpert47@gmail.com', 'password', 'https://s3.amazonaws.com/uifaces/faces/twitter/larrygerard/128.jpg');
+INSERT INTO users (email, password, profile_picture)VALUES('Wilma_Hilpert47@gmail.com', 'password', 'https://s3.amazonaws.com/uifaces/faces/twitter/larrygerard/128.jpg');
+
