f85858b6e9d8eec45cfb258e1211bee7e61aed10
QuintenAiton <quinaiton@hotmail.com>
2021-02-06 23:03:35 +0000

    Fixes all Users Having Same Info

--- db/seeds/userSeedWriter.js
+++ db/seeds/userSeedWriter.js
@@ -1,21 +1,20 @@
 const faker = require("faker");
 const fs = require("fs");
-const user = {
-  randomEmail: `${faker.internet.email()}`,
-  userPass: "password",
-  randomPicture: `${faker.image.avatar()}`,
-};
 const users = [];
 
 const seedUsers = () => {
   let x = 0;
   while (x <= 5) {
-    const queryString = `INSERT INTO users(email, password, profile_picture)Values('${user.randomEmail}', '${user.userPass}', '${user.randomPicture}')`;
+    const user = {
+      randomEmail: faker.internet.email(),
+      userPass: "password",
+      randomPicture: faker.image.avatar(),
+    };
+    const queryString = `INSERT INTO users(email, password, profile_picture)Values('${user.randomEmail}', '${user.userPass}', '${user.randomPicture}');`;
     users.push(queryString);
     x++;
   }
   return users;
 };
 
-fs.writeFileSync("db/seeds/userSeeds_2.sql", seedUsers());
-seedUsers();
+fs.writeFileSync("db/seeds/userSeeds.sql", seedUsers());
