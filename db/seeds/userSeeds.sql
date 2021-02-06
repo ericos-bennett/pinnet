b6c53e6a4e3566b5ab996fa7dddb162e0e8c1aa9
QuintenAiton <quinaiton@hotmail.com>
2021-02-06 23:04:21 +0000

    All Users Have Different Info

--- db/seeds/userSeeds.sql
+++ db/seeds/userSeeds.sql
@@ -1,7 +1,6 @@
-INSERT INTO users (email, password, profile_picture)VALUES('Wilma_Hilpert47@gmail.com', 'password', 'https://s3.amazonaws.com/uifaces/faces/twitter/larrygerard/128.jpg');
-INSERT INTO users (email, password, profile_picture)VALUES('Wilma_Hilpert47@gmail.com', 'password', 'https://s3.amazonaws.com/uifaces/faces/twitter/larrygerard/128.jpg');
-INSERT INTO users (email, password, profile_picture)VALUES('Wilma_Hilpert47@gmail.com', 'password', 'https://s3.amazonaws.com/uifaces/faces/twitter/larrygerard/128.jpg');
-INSERT INTO users (email, password, profile_picture)VALUES('Wilma_Hilpert47@gmail.com', 'password', 'https://s3.amazonaws.com/uifaces/faces/twitter/larrygerard/128.jpg');
-INSERT INTO users (email, password, profile_picture)VALUES('Wilma_Hilpert47@gmail.com', 'password', 'https://s3.amazonaws.com/uifaces/faces/twitter/larrygerard/128.jpg');
-INSERT INTO users (email, password, profile_picture)VALUES('Wilma_Hilpert47@gmail.com', 'password', 'https://s3.amazonaws.com/uifaces/faces/twitter/larrygerard/128.jpg');
-
+INSERT INTO users(email, password, profile_picture)Values('Theresia.Blanda@hotmail.com', 'password', 'https://s3.amazonaws.com/uifaces/faces/twitter/benoitboucart/128.jpg');
+INSERT INTO users(email, password, profile_picture)Values('Antonietta28@gmail.com', 'password', 'https://s3.amazonaws.com/uifaces/faces/twitter/doooon/128.jpg');
+INSERT INTO users(email, password, profile_picture)Values('Lambert46@gmail.com', 'password', 'https://s3.amazonaws.com/uifaces/faces/twitter/dreizle/128.jpg');
+INSERT INTO users(email, password, profile_picture)Values('Jaleel.Beer@hotmail.com', 'password', 'https://s3.amazonaws.com/uifaces/faces/twitter/depaulawagner/128.jpg');
+INSERT INTO users(email, password, profile_picture)Values('Billie.Doyle56@hotmail.com', 'password', 'https://s3.amazonaws.com/uifaces/faces/twitter/meelford/128.jpg');
+INSERT INTO users(email, password, profile_picture)Values('Antonio35@yahoo.com', 'password', 'https://s3.amazonaws.com/uifaces/faces/twitter/mfacchinello/128.jpg');
