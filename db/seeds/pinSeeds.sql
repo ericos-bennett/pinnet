4e03462ccb5f530b0a14ba735f2e119b6cab3b3e
QuintenAiton <quinaiton@hotmail.com>
2021-02-07 00:12:56 +0000

    Querys For Seeding Pin DB

---
+++ db/seeds/pinSeeds.sql
@@ -0,0 +1,59 @@
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(6,7, 'Principal Accountability Technician', 'http://placeimg.com/640/480','Eligendi amet reiciendis maxime officiis repellat est.', 'http://placeimg.com/640/480/business');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(2,5, 'Central Data Associate', 'http://placeimg.com/640/480','Molestiae aperiam deserunt eveniet velit unde perferendis rerum rerum vitae.', 'http://placeimg.com/640/480/technics');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(3,7, 'Dynamic Integration Supervisor', 'http://placeimg.com/640/480','Earum qui sunt.', 'http://placeimg.com/640/480/cats');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(6,6, 'Central Solutions Associate', 'http://placeimg.com/640/480','Id omnis quidem id est temporibus adipisci magnam deserunt suscipit.', 'http://placeimg.com/640/480/transport');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(5,7, 'Future Security Liaison', 'http://placeimg.com/640/480','Nihil quasi quia voluptas voluptas quisquam non a.', 'http://placeimg.com/640/480/city');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(1,1, 'Central Division Technician', 'http://placeimg.com/640/480','Accusantium consequuntur aspernatur deserunt.', 'http://placeimg.com/640/480/transport');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(6,1, 'Product Solutions Designer', 'http://placeimg.com/640/480','Sit molestiae tempore ut unde distinctio sunt sunt officia.', 'http://placeimg.com/640/480/nature');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(1,4, 'Corporate Marketing Administrator', 'http://placeimg.com/640/480','In rerum rerum est quis nam assumenda placeat rerum.', 'http://placeimg.com/640/480/people');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(5,7, 'Forward Intranet Technician', 'http://placeimg.com/640/480','Laudantium nihil explicabo illum atque hic doloribus dolorem ipsum.', 'http://placeimg.com/640/480/nature');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(6,4, 'Dynamic Markets Planner', 'http://placeimg.com/640/480','Quia provident consequuntur.', 'http://placeimg.com/640/480/cats');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(2,4, 'Chief Directives Analyst', 'http://placeimg.com/640/480','Reprehenderit dicta nulla corrupti qui eos itaque omnis inventore et.', 'http://placeimg.com/640/480/business');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(4,1, 'Forward Metrics Administrator', 'http://placeimg.com/640/480','Dicta quas illo minima ea.', 'http://placeimg.com/640/480/cats');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(4,7, 'Direct Brand Architect', 'http://placeimg.com/640/480','Voluptas iste corrupti ut ex beatae facere rerum.', 'http://placeimg.com/640/480/nature');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(2,6, 'Customer Response Strategist', 'http://placeimg.com/640/480','Accusamus fuga voluptas.', 'http://placeimg.com/640/480/business');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(2,1, 'Customer Quality Orchestrator', 'http://placeimg.com/640/480','Voluptate molestias rerum voluptas velit aperiam illo.', 'http://placeimg.com/640/480/abstract');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(6,4, 'Regional Group Strategist', 'http://placeimg.com/640/480','Corrupti in minus sed molestiae saepe vel.', 'http://placeimg.com/640/480/technics');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(2,2, 'Global Usability Designer', 'http://placeimg.com/640/480','Et accusamus nam qui dolores voluptatem.', 'http://placeimg.com/640/480/nature');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(5,6, 'International Interactions Supervisor', 'http://placeimg.com/640/480','Nobis magnam velit consectetur tempore.', 'http://placeimg.com/640/480/city');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(4,6, 'Direct Mobility Planner', 'http://placeimg.com/640/480','Veniam aut qui.', 'http://placeimg.com/640/480/food');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(6,6, 'Principal Assurance Producer', 'http://placeimg.com/640/480','Et voluptates ipsa quod voluptas pariatur ut.', 'http://placeimg.com/640/480/abstract');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(4,5, 'Internal Optimization Manager', 'http://placeimg.com/640/480','Iure eveniet cumque ad delectus et quia sit dicta.', 'http://placeimg.com/640/480/people');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(1,1, 'Senior Operations Administrator', 'http://placeimg.com/640/480','Voluptatem dolores blanditiis aut facilis est natus iusto quas similique.', 'http://placeimg.com/640/480/business');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(4,4, 'Product Configuration Orchestrator', 'http://placeimg.com/640/480','Eos voluptatem aut.', 'http://placeimg.com/640/480/nightlife');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(5,3, 'Customer Tactics Producer', 'http://placeimg.com/640/480','Quasi delectus atque.', 'http://placeimg.com/640/480/animals');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(5,6, 'Direct Operations Producer', 'http://placeimg.com/640/480','Error qui et sit odit.', 'http://placeimg.com/640/480/fashion');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(5,4, 'Product Solutions Agent', 'http://placeimg.com/640/480','Eos et dolores aut ut aliquid voluptate eum voluptatum atque.', 'http://placeimg.com/640/480/cats');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(4,2, 'Future Division Technician', 'http://placeimg.com/640/480','Saepe dolores est tempora molestiae sint.', 'http://placeimg.com/640/480/technics');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(3,5, 'Principal Web Technician', 'http://placeimg.com/640/480','Quia iste mollitia earum sed aut et.', 'http://placeimg.com/640/480/fashion');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(5,6, 'Dynamic Web Director', 'http://placeimg.com/640/480','Tempore quo quo.', 'http://placeimg.com/640/480/nature');
+
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(2,3, 'Future Data Analyst', 'http://placeimg.com/640/480','Ad amet deleniti.', 'http://placeimg.com/640/480/city');
