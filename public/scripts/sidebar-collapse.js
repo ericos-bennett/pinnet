e6deb7e23c0aff49420168eba02b8c653c785d3a
Melanie Tong <melaniietong@gmail.com>
2021-02-07 02:41:53 +0000

    Fix class names

--- public/scripts/sidebar-collapse.js
+++ public/scripts/sidebar-collapse.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
-  $("#sidebar").mCustomScrollbar({
+  $(".sidebar").mCustomScrollbar({
       theme: "minimal"
   });
 
   $('#sidebarCollapse').on('click', function () {
-      $('#sidebar, #content').toggleClass('active');
+      $('.sidebar, .content').toggleClass('active');
       $('.collapse.in').toggleClass('in');
       $('a[aria-expanded=true]').attr('aria-expanded', 'false');
   });
