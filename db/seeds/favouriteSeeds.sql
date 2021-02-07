c03f479fa6b28068ef02d873acd857aefe151c00
QuintenAiton <quinaiton@hotmail.com>
2021-02-07 03:11:25 +0000

    Seeds  for Favourites db

---
+++ db/seeds/favouriteSeeds.sql
@@ -0,0 +1,62 @@
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(2,17, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(6,7, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(3,12, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(2,11, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(6,14, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(6,3, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(5,27, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(2,21, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(2,22, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(3,29, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(6,9, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(1,2, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(3,30, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(1,11, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(5,16, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(6,12, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(2,28, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(4,7, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(6,24, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(6,5, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(3,27, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(1,12, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(4,28, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(5,29, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(6,12, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(2,21, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(2,16, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(6,10, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(4,13, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(6,29, true);
+INSERT INTO favourites(user_id, pin_id, is_liked)
+Values(1,6, true);
