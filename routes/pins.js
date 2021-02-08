77165674a931b3b66e7b25bc743e4db79261c45d
Eric <ericos.bennett@gmail.com>
2021-02-08 12:18:45 -0800

    search route query (still need to add username)

--- routes/pins.js
+++ routes/pins.js
@@ -10,7 +10,34 @@ module.exports = (db) => {
 
   // Route for the search bar ('/pins')
   router.get('/', (req, res) => {
-    // do something...
+
+
+    const search = `%${req.query.search}%`.toLowerCase();
+    console.log(search);
+
+    const queryString = `
+      SELECT pins.id AS pins_id, user_id, url, title, description, media, name AS topic, created_at
+      FROM pins
+      JOIN topics ON topic_id = topics.id
+      JOIN users ON user_id = users.id
+      WHERE LOWER(name) LIKE $1
+        OR LOWER(title) LIKE $1
+        OR LOWER(description) LIKE $1
+      ORDER BY created_at DESC;
+    ;`;
+    const values = [search];
+
+    db.query(queryString, values)
+      .then(data => {
+        const pins = data.rows;
+        res.json({ pins });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+
   });
 
   // Add pins to my wall
