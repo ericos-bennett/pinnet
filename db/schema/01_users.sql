f610deddcf8d0a55da62477f6c3abc37e5114067
Melanie Tong <melaniietong@gmail.com>
2021-02-09 07:58:15 +0000

    Remove NOT NULL from profile_picture

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -5,5 +5,5 @@ CREATE TABLE users (
   username VARCHAR(30) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  profile_picture VARCHAR(255) NOT NULL
+  profile_picture VARCHAR(255)
 );
