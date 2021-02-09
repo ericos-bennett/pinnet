aac77cca0f52d9acb501fd4910c78ddad9a9d3b0
Eric <ericos.bennett@gmail.com>
2021-02-08 21:17:26 -0800

    put and delete requests for pins

--- routes/pins.js
+++ routes/pins.js
@@ -43,6 +43,7 @@ module.exports = (db) => {
   router.post('/', (req, res) => {
 
     // Replace this dummy user_id with the one specified in the user's session
+    // Write some code to validate that the request comes form a logged in user
     const userId = 1;
     const pinData = {...req.body, userId};
 
@@ -52,8 +53,6 @@ module.exports = (db) => {
       RETURNING *;
       `;
     const values = [pinData.userId, pinData.topicId, pinData.title, pinData.url, pinData.description, pinData.media];
-
-    console.log(values);
 
     db.query(queryString, values)
       .then(data => {
@@ -70,12 +69,89 @@ module.exports = (db) => {
 
   // Edit pins on my wall (if the passed user_id is it's owner)
   router.put('/:pin_id', (req, res) => {
-    // do something
+
+    // Replace this dummy user_id with the one specified in the user's session
+    // Write some code to validate that the request comes from the user who made the pin
+    const userId = 1;
+
+    // If the request doesn't contain any of the appropriate values, redirect
+    if (!(req.body.url || req.body.title || req.body.description || req.body.media || req.body.topic)) {
+      res.redirect('/');
+
+    } else {
+      let queryString = `
+        UPDATE pins
+        SET `;
+
+      // For each param passed, concatenate a clause
+      if (req.body.url) queryString += `url = '${req.body.url}', `;
+      if (req.body.title) queryString += `title = '${req.body.title}', `;
+      if (req.body.description) queryString += `description = '${req.body.description}', `;
+      if (req.body.media) queryString += `media = '${req.body.media}', `;
+
+      // fix this when/if there's time, right now you can't edit the pin's topic
+      // because it's asynchronous, these queries runs AFTER the main update
+      if (req.body.topic) {
+
+        // let topicId;
+        // db.query(`
+        //   SELECT id FROM topics
+        //   WHERE name = '${req.body.topic}';`)
+        //   .then(data => {
+        //     if (data.rows[0]) {
+        //       topicId = data.rows[0].id;
+        //     } else {
+
+        //       // make a new record in the topics table and return its id
+        //       db.query(`
+        //         INSERT INTO topics (name)
+        //         VALUES ('${req.body.topic}')
+        //         RETURNING *;`)
+        //         .then(data => {
+        //           topicId = data.rows[0].id;
+        //         })
+        //         .catch(err => console.log(err));
+        //     }
+        //   })
+        //   .catch(err => console.log(err))
+        //   .then(console.log('topicId', topicId));
+
+        // queryString += `topic_id = '${topicId}', `
+      }
+
+      // Remove the last comma and space
+      queryString = queryString.slice(0, -2);
+
+      // Add the WHERE specifying the record, return that record
+      queryString += `
+        WHERE id = '${req.params.pin_id}'
+        RETURNING *;`;
+
+      db.query(queryString)
+        .then(data => {
+          const updatedPin = data.rows[0];
+          res.json({ updatedPin });
+        })
+        .catch(err => {
+          res
+            .status(500)
+            .json({ error: err.message });
+        });
+    }
   });
 
   // Delete pins on my wall (if the passed user_id is it's owner)
   router.delete('/:pin_id', (req, res) => {
-    // do something
+
+    const pinId = req.params.pin_id;
+    const queryString = `
+      DELETE FROM pins
+      WHERE id = $1;
+    `;
+
+    db.query(queryString, [pinId])
+      .then(() => res.end())
+      .catch(err => console.log(err));
   });
 
   // Leave a comment on someone's pin
