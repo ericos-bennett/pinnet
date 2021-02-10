675d2f4c43ecb2cc8c9385bd8117e9b11d2f2478
Eric <ericos.bennett@gmail.com>
2021-02-10 11:09:18 -0800

    new route to add a pin to a topic

--- routes/pins.js
+++ routes/pins.js
@@ -37,7 +37,7 @@ module.exports = (db) => {
         const pins = data.rows;
         const userId = req.cookies.userId;
         const page = "explore";
-        const searchTerm = search.substring(1, search.length-1);
+        const searchTerm = search.substring(1, search.length - 1);
         res.render("index", { pins, userId, page, searchTerm});
       })
       .catch(err => {
@@ -59,11 +59,11 @@ module.exports = (db) => {
     if (pinData.userId && pinData.title && pinData.url && pinData.media) {
 
       const queryString = `
-      INSERT INTO pins(user_id, topic_id, title, url, description, media)
-      VALUES($1, $2, $3, $4, $5, $6)
+      INSERT INTO pins(user_id, title, url, description, media)
+      VALUES($1, $2, $3, $4, $5)
       RETURNING *;
       `;
-      const values = [pinData.userId, pinData.topicId, pinData.title, pinData.url, pinData.description, pinData.media];
+      const values = [pinData.userId, pinData.title, pinData.url, pinData.description, pinData.media];
 
       db.query(queryString, values)
         .then(data => {
@@ -96,36 +96,6 @@ module.exports = (db) => {
     if (req.body.title) queryString += `title = '${req.body.title}', `;
     if (req.body.description) queryString += `description = '${req.body.description}', `;
     if (req.body.media) queryString += `media = '${req.body.media}', `;
-
-    // fix this when/if there's time, right now you can't edit the pin's topic
-    // because it's asynchronous, these queries runs AFTER the main db update
-    // if (req.body.topic) {
-
-    // let topicId;
-    // db.query(`
-    //   SELECT id FROM topics
-    //   WHERE name = '${req.body.topic}';`)
-    //   .then(data => {
-    //     if (data.rows[0]) {
-    //       topicId = data.rows[0].id;
-    //     } else {
-
-    //       // make a new record in the topics table and return its id
-    //       db.query(`
-    //         INSERT INTO topics (name)
-    //         VALUES ('${req.body.topic}')
-    //         RETURNING *;`)
-    //         .then(data => {
-    //           topicId = data.rows[0].id;
-    //         })
-    //         .catch(err => console.log(err));
-    //     }
-    //   })
-    //   .catch(err => console.log(err))
-    //   .then(console.log('topicId', topicId));
-
-    // queryString += `topic_id = '${topicId}', `
-    // }
 
     // Remove the last comma and space
     queryString = queryString.slice(0, -2);
@@ -223,6 +193,50 @@ module.exports = (db) => {
     } else {
       res.end();
     }
+
+  });
+
+  // Add a topic to a pin
+  router.post('/:pin_id/topic', (req, res) => {
+
+    const topic = req.body.topic;
+
+    // Add a new topic to the topics table and return its id
+    const queryString = `
+      INSERT INTO topics (name)
+      VALUES ($1)
+      ON CONFLICT (name)
+      DO UPDATE
+      SET name = $1
+      RETURNING id;
+    `;
+    const values = [topic];
+
+    console.log(topic);
+
+    db.query(queryString, values)
+      .then(data => {
+        const topicId = data.rows[0].id;
+        const pinId = req.params.pin_id;
+        const userId = req.cookies.userId;
+
+        // Update the pins table with the new topic_id
+        const queryString = `
+          UPDATE pins
+          SET topic_id = $1
+          WHERE id = $2 AND user_id = $3
+          RETURNING *
+          `;
+        const values = [topicId, pinId, userId];
+
+        db.query(queryString, values)
+          .then(data => {
+            const updatedPin = data.rows[0];
+            res.json(updatedPin);
+          })
+          .catch(err => console.log(err));
+      })
+      .catch(err => console.log(err));`
 
   });
 
