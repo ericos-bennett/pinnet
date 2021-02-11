8477c63fbca85906f665b58bab21af6696c109f0
Eric <ericos.bennett@gmail.com>
2021-02-11 22:47:48 +0000

    form and route for adding a new topic

--- routes/users.js
+++ routes/users.js
@@ -39,5 +39,28 @@ module.exports = (db) => {
       });
   });
 
+  // Adds a new topic (right now they're universal, TBD user-specific)
+  router.post('/topics', (req, res) => {
+
+    const newTopic = req.body.topic;
+
+    console.log(newTopic);
+
+    const queryString = `
+      INSERT INTO topics (name)
+      VALUES ($1)
+      ON CONFLICT (name)
+      DO NOTHING;
+    `;
+    const values = [newTopic];
+
+    db.query(queryString, values)
+      .then(() => {
+        const userId = req.cookies.userId;
+        res.redirect(`/users/${userId}`);
+      })
+      .catch(err => console.log(err));
+  });
+
   return router;
 };
