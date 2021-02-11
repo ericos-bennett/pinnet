03397eee1cb6c395a23e7d154cf3489f2173c7d5
QuintenAiton <quinaiton@hotmail.com>
2021-02-11 17:17:15 +0000

    No real changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,22 +1,20 @@
-$(function() {
-
+$(function () {
   // Like icon event handler, you can't currently unlike
-  $('.like').on('click', function(e) {
+  $(".like").on("click", function (e) {
     const likeIcon = this;
-    const pin = this.parentElement.parentElement;
+    const pin = this.parentElement.parentElement.parentElement;
     const pinId = pin.id.slice(3);
     console.log(pinId);
 
     $.ajax({
       url: `pins/${pinId}/like`,
-      type: 'POST',
+      type: "POST",
       data: {},
       contentType: "application/json; charset=utf-8",
       dataType: "json",
-    }).always(function(data) {
+    }).always(function (data) {
       $(likeIcon).removeClass("far");
       $(likeIcon).addClass("fas");
     });
   });
-
 });
