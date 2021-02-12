7ff5302b85d59ae3732dd34fe086aa63c07e107b
Eric <ericos.bennett@gmail.com>
2021-02-12 03:24:12 +0000

    updated seeds with real data

--- db/seeds/03_pinSeeds.sql
+++ db/seeds/03_pinSeeds.sql
@@ -1,62 +1,32 @@
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(6,7, 'Principal Accountability Technician', 'http://example.edu/1','Eligendi amet reiciendis maxime officiis repellat est.', 'http://placeimg.com/640/480/business');
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(6,null, 'Dribbblisation of Design', 'https://medium.com/intercom-inside/the-dribbblisation-of-design-406422ccb026','There are divergent things happening in the product and interaction design community. On one hand, we have some amazing pieces of writing from the likes of Ryan Singer and Julie Zhuo, moving our craft forward. On the other hand, we have a growing number of people posting and discussing their work on Dribbble, the aggregated results of which are moving our craft backwards.', 'https://miro.medium.com/max/740/1*s1t1fQQrogEQfwRHpvKDYw.png');
 
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(2,5, 'Central Data Associate', 'http://example.edu/2','Molestiae aperiam deserunt eveniet velit unde perferendis rerum rerum vitae.', 'http://placeimg.com/640/480/technics');
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(2,null, 'A Complete Guide to Flexbox', 'https://css-tricks.com/snippets/css/a-guide-to-flexbox/','Our comprehensive guide to CSS flexbox layout. This complete guide explains everything about flexbox, focusing on all the different possible properties for the parent element (the flex container) and the child elements (the flex items). It also includes history, demos, patterns, and a browser support chart.', 'https://i2.wp.com/css-tricks.com/wp-content/uploads/2018/12/guide-flexbox.png?fit=1200%2C600&ssl=1');
 
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(3,7, 'Unicode.org', 'https://home.unicode.org/','Everyone in the world should be able to use their own language on phones and computers.', 'https://home.unicode.org/wp-content/uploads/2019/12/Unicode-Logo-Final-Blue-95x112.jpg');
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(3,null, 'Unicode.org', 'https://home.unicode.org/','Everyone in the world should be able to use their own language on phones and computers.', 'https://home.unicode.org/wp-content/uploads/2019/12/Unicode-Logo-Final-Blue-95x112.jpg');
 
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(6,6, 'Network Programming', 'https://beej.us/guide/bgnet/html/','Hey! Socket programming got you down? Is this stuff just a little too difficult to figure out from the man pages? You want to do cool Internet programming, but you don’t have time to wade through a gob of structs trying to figure out if you have to call bind() before you connect(), etc., etc.', 'http://placeimg.com/640/480/animals');
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(6,null, 'The Modern Web Browser', 'https://developers.google.com/web/updates/2018/09/inside-browser-part1','In this 4-part blog series, we’ll look inside the Chrome browser from high-level architecture to the specifics of the rendering pipeline. If you ever wondered how the browser turns your code into a functional website, or you are unsure why a specific technique is suggested for performance improvements, this series is for you.', 'https://developers.google.com/web/images/social-webfu-16x9.png');
 
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(5,7, 'Latency', 'http://calpaterson.com/latency.html','What''s the fastest place to put my server? Beyond the time taken for servers to respond to requests it takes time just to traverse the internet, just to get a packet from A to B.
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(5,null, 'Latency', 'http://calpaterson.com/latency.html','What''s the fastest place to put my server? Beyond the time taken for servers to respond to requests it takes time just to traverse the internet, just to get a packet from A to B.
 
 To estimate what the theoretical best physical place to put my own server is I''ve combined publicly available data on latencies with my own web server accesslogs. I''m aiming to get a rough, quantitative, answer that''s based on a real data set.', 'http://calpaterson.com/assets/network-transit-times.png');
 
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(1,1, 'API Directory', 'https://www.programmableweb.com/apis/directory','A handly resource of publicly available APIs to get inspiration from and experiment with.', 'https://www.programmableweb.com/sites/all/themes/pw_bootstrap_two/images/logo-65x65.png');
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(1,null, 'API Directory', 'https://www.programmableweb.com/apis/directory','A handly resource of publicly available APIs to get inspiration from and experiment with.', 'https://www.programmableweb.com/sites/all/themes/pw_bootstrap_two/images/logo-65x65.png');
 
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(6,1, 'Feather Icons', 'https://feathericons.com/','Simply beautiful open source icons
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(6,null, 'Feather Icons', 'https://feathericons.com/','Simply beautiful open source icons
 ', 'https://feathericons.com/feather-og.png');
 
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(1,4, 'How to open a file in Emacs', 'https://www.murilopereira.com/how-to-open-a-file-in-emacs/','I''ve recently joined a company that for security reasons doesn’t allow their source code on laptops. Development happens strictly on workstations inside their private networks, with some using their text editor''s support for remote file editing and others running editors on those machines via SSH.', 'https://www.murilopereira.com/tron.jpg');
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(1,null, 'How to open a file in Emacs', 'https://www.murilopereira.com/how-to-open-a-file-in-emacs/','I''ve recently joined a company that for security reasons doesn’t allow their source code on laptops. Development happens strictly on workstations inside their private networks, with some using their text editor''s support for remote file editing and others running editors on those machines via SSH.', 'https://www.murilopereira.com/tron.jpg');
 
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(5,7, 'Forward Intranet Technician', 'http://example.edu/9','Laudantium nihil explicabo illum atque hic doloribus dolorem ipsum.', 'http://placeimg.com/640/480/nature');
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(5,null, 'Pico de Gallo', 'https://cookieandkate.com/images/2018/09/best-pico-de-gallo-recipe-2.jpg','Do you love pico de gallo like I do? It’s a classic Mexican tomato dip (or sauce) that adds a fresh, healthy, low-calorie boost of flavor to just about any Mexican meal.', 'https://cookieandkate.com/images/2018/09/best-pico-de-gallo-recipe-2.jpg');
 
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(6,4, 'Dynamic Markets Planner', 'http://example.edu/10','Quia provident consequuntur.', 'http://placeimg.com/640/480/cats');
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(6,null, 'Virtual Power Plants', 'https://121gigawatts.org/what-is-a-virtual-power-plant-anyway/','Here’s something I had been completely clueless about, before I started working in the energy industry: How can it be that I can just decide to start my washing machine or a hair dryer at any time, and no one cares, i.e. the power outlet just covers it. Like where does that extra energy suddenly come from? Surely that far-away power plant didn’t increase its production just to cover that extra energy consumption I just caused.', 'https://121gigawatts.org/content/images/2020/05/teslas-1.gif');
 
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(2,4, 'Chief Directives Analyst', 'http://example.edu/11','Reprehenderit dicta nulla corrupti qui eos itaque omnis inventore et.', 'http://placeimg.com/640/480/business');
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(2,null, 'Federer as Religion', 'http://example.edu/11https://www.nytimes.com/2006/08/20/sports/playmagazine/20federer.html?pagewanted=all','Almost anyone who loves tennis and follows the men’s tour on television has, over the last few years, had what might be termed Federer Moments. These are times, as you watch the young Swiss play, when the jaw drops and eyes protrude and sounds are made that bring spouses in from other rooms to see if you’re O.K.', 'https://static01.nyt.com/newsgraphics/images/icons/defaultPromoCrop.png?year=2006');
 
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(4,1, 'Forward Metrics Administrator', 'http://example.edu/12','Dicta quas illo minima ea.', 'http://placeimg.com/640/480/cats');
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(4,null, 'The Horse-Racing Code', 'https://www.bloomberg.com/news/features/2018-05-03/the-gambler-who-cracked-the-horse-racing-code','Bill Benter did the impossible: He wrote an algorithm that couldn’t lose at the track. Close to a billion dollars later, he tells his story for the first time.', 'https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iE1QfLq8Hrcs/v0/1200x900.jpg');
 
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(4,7, 'Direct Brand Architect', 'http://example.edu/13','Voluptas iste corrupti ut ex beatae facere rerum.', 'http://placeimg.com/640/480/nature');
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(4,null, 'Chess Openings', 'https://www.chess.com/openings','Search more than 3,000 opening lines. Find the perfect chess opening for you. Explore variations and master chess games.', 'https://betacssjs.chesscomfiles.com/bundles/web/images/social/share-openings.5d6de869.png');
 
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(2,6, 'Customer Response Strategist', 'http://example.edu/14','Accusamus fuga voluptas.', 'http://placeimg.com/640/480/business');
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(2,null, 'Arduino Project Hub', 'https://create.arduino.cc/projecthub','Arduino is an open-source electronics prototyping platform based on flexible, easy-to-use hardware and software. Come explore Arduino projects!', 'https://hackster.imgix.net/uploads/attachments/1255992/_v4Lt2oARGK.blob?auto=compress%2Cformat&w=900&h=675&fit=min');
 
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(2,1, 'Customer Quality Orchestrator', 'http://example.edu/15','Voluptate molestias rerum voluptas velit aperiam illo.', 'http://placeimg.com/640/480/abstract');
-
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(6,4, 'Regional Group Strategist', 'http://example.edu/16','Corrupti in minus sed molestiae saepe vel.', 'http://placeimg.com/640/480/technics');
-
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(2,2, 'Global Usability Designer', 'http://example.edu/17','Et accusamus nam qui dolores voluptatem.', 'http://placeimg.com/640/480/nature');
-
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(5,6, 'International Interactions Supervisor', 'http://example.edu/18','Nobis magnam velit consectetur tempore.', 'http://placeimg.com/640/480/city');
-
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(4,6, 'Direct Mobility Planner', 'http://example.edu/19','Veniam aut qui.', 'http://placeimg.com/640/480/food');
-
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(6,6, 'Principal Assurance Producer', 'http://example.edu/20','Et voluptates ipsa quod voluptas pariatur ut.', 'http://placeimg.com/640/480/abstract');
-
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(4,5, 'Internal Optimization Manager', 'http://example.edu/21','Iure eveniet cumque ad delectus et quia sit dicta.', 'http://placeimg.com/640/480/people');
-
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(1,1, 'Senior Operations Administrator', 'http://example.edu/22','Voluptatem dolores blanditiis aut facilis est natus iusto quas similique.', 'http://placeimg.com/640/480/business');
-
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(4,4, 'Product Configuration Orchestrator', 'http://example.edu/23','Eos voluptatem aut.', 'http://placeimg.com/640/480/nightlife');
-
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(5,3, 'Customer Tactics Producer', 'http://example.edu/24','Quasi delectus atque.', 'http://placeimg.com/640/480/animals');
-
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(5,6, 'Direct Operations Producer', 'http://example.edu/25','Error qui et sit odit.', 'http://placeimg.com/640/480/fashion');
-
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(5,4, 'Product Solutions Agent', 'http://example.edu/26','Eos et dolores aut ut aliquid voluptate eum voluptatum atque.', 'http://placeimg.com/640/480/cats');
-
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(4,2, 'Future Division Technician', 'http://example.edu/27','Saepe dolores est tempora molestiae sint.', 'http://placeimg.com/640/480/technics');
-
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(3,5, 'Principal Web Technician', 'http://example.edu/28','Quia iste mollitia earum sed aut et.', 'http://placeimg.com/640/480/fashion');
-
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(5,6, 'Dynamic Web Director', 'http://example.edu/29','Tempore quo quo.', 'http://placeimg.com/640/480/nature');
-
-INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(2,3, 'Future Data Analyst', 'http://example.edu/30','Ad amet deleniti.', 'http://placeimg.com/640/480/city');
+INSERT INTO pins(user_id,topic_id,title,url,description,media)VALUES(2,null, '80,000 Hours', 'https://80000hours.org/','How can you best use them to help solve the world’s most pressing problems? We’re a nonprofit that does research to answer this question. We provide free advice and support to help you have a greater impact with your career.', 'https://cdn.80000hours.org/wp-content/uploads/2015/12/og-image1.jpg');
--- db/seeds/05_favouriteSeeds.sql
+++ db/seeds/05_favouriteSeeds.sql
@@ -1,5 +1,5 @@
 INSERT INTO favourites(user_id, pin_id, is_liked)
-Values(2,17, true);
+Values(2,9, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
 Values(6,7, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
@@ -11,48 +11,48 @@ Values(6,14, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
 Values(6,3, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
-Values(5,27, true);
+Values(5,6, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
-Values(2,21, true);
+Values(2,13, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
-Values(2,22, true);
+Values(2,2, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
-Values(3,29, true);
+Values(3,15, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
-Values(6,9, true);
+Values(6,11, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
 Values(1,2, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
-Values(3,30, true);
+Values(3,3, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
 Values(1,11, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
-Values(5,16, true);
+Values(5,15, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
 Values(6,12, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
-Values(2,28, true);
+Values(2,8, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
 Values(4,7, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
-Values(6,24, true);
+Values(6,4, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
 Values(6,5, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
-Values(3,27, true);
+Values(3,13, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
 Values(1,12, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
-Values(4,28, true);
+Values(4,8, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
-Values(5,29, true);
+Values(5,9, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
-Values(2,16, true);
+Values(2,6, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
 Values(6,10, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
 Values(4,13, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
-Values(6,29, true);
+Values(6,9, true);
 INSERT INTO favourites(user_id, pin_id, is_liked)
 Values(1,6, true);
